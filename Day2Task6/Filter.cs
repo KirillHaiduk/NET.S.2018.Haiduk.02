57437cd6ea96b4868440dc935f3af74f16f5ad01
KirillHaiduk <33031000+KirillHaiduk@users.noreply.github.com>
2018-04-18 16:23:25 +0300

    Update Filter.cs

--- Day2Task6/Filter.cs
+++ Day2Task6/Filter.cs
@@ -6,69 +6,34 @@ namespace Day2Task6
     /// <summary>
     /// The filtering class
     /// </summary>
-    public class Filter
-    {
-        #region Public members
+    public static class Filter
+    {        
         /// <summary>
-        /// Method which takes an array of integers and filters it so that only numbers containing the given digit remain on the output
+        /// Filters a sequence of values based on a predicate
         /// </summary>
-        /// <param name="array">array that is necessary to filter</param>
-        /// <param name="digit">filtering digit</param>
-        /// <returns>Array of integers containing filtering digit</returns>
-        public static int[] FilterDigit(int[] array, int digit)
+        /// <typeparam name="TSource">The type of the elements of sequence</typeparam>
+        /// <param name="source">Sequence to filter</param>
+        /// <param name="filter">A function to test each element for a condition</param>
+        /// <returns>Sequence that contains elements from the input sequence that satisfy the condition</returns>
+        public static IEnumerable<TSource> FindAll<TSource> (this IEnumerable<TSource> source, Func<TSource, bool> filter)
         {
-            if (digit > 9 || digit < 0)
+            if (source == null)
             {
-                throw new ArgumentOutOfRangeException($"{nameof(digit)} must be from 0 to 9");
+                throw new ArgumentNullException(nameof(source));
             }
 
-            if (array.Length == 0)
+            if (filter == null)
             {
-                throw new ArgumentNullException($"{nameof(array)} is empty.");
+                throw new ArgumentNullException(nameof(source));
             }
 
-            List<int> list = new List<int>();
-            for (int i = 0; i < array.Length; i++)
+            foreach (var t in source)
             {
-                if (ContainsDigit(array[i], digit))
+                if (filter(t))
                 {
-                    list.Add(array[i]);
+                    yield return t;
                 }
             }
-
-            if (list.Count == 0)
-            {
-                return null;
-            }
-
-            return list.ToArray();
-        }
-        #endregion
-
-        #region Private members
-        /// <summary>
-        /// Method which returns if accepted item of array contains digit
-        /// </summary>
-        /// <param name="item">Item of given array</param>
-        /// <param name="digit">Filtering digit</param>
-        /// <returns>True or false depending on whether item contains digit</returns>
-        private static bool ContainsDigit(int item, int digit)
-        {
-            int temp = item;
-
-            for (int i = 0; i < item.ToString().Length; i++)
-            {
-                var divisionResult = temp / 10;
-                int remainder = temp % 10;
-                temp = divisionResult;
-                if (Math.Abs(remainder) == digit)
-                {
-                    return true;
-                }
-            }
-
-            return false;
-        }
-        #endregion
+        }        
     }
 }
